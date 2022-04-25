42b7098e8fa59f351061abf4284f15dba3e7f14b
xxxxxxH <425270071@qq.com>
2022-04-25 21:11:35 +0800

    2022年4月25日 21:11:30

--- mobile/src/main/java/fear/of/god/ui/ResultActivity.kt
+++ mobile/src/main/java/fear/of/god/ui/ResultActivity.kt
@@ -1,33 +1,45 @@
 package fear.of.god.ui
 
 import android.annotation.SuppressLint
+import android.widget.ImageView
 import android.widget.TextView
 import com.github.shadowsocks.R
 import fear.of.god.base.BaseActivity
 import fear.of.god.tools.log
 
-class ResultActivity:BaseActivity(R.layout.layout_result) {
+class ResultActivity : BaseActivity(R.layout.layout_result) {
     private val status by lazy {
         intent.getStringExtra("status")
     }
+
+    private lateinit var imageResult: ImageView
 
     @SuppressLint("SetTextI18n")
     override fun initView() {
         super.initView()
         "$status".log()
+        imageResult = findViewById(R.id.imageResult)
+        findViewById<ImageView>(R.id.back).apply {
+            setOnClickListener { finish() }
+        }
+        findViewById<TextView>(R.id.title).apply {
+            text = "Result"
+        }
         findViewById<TextView>(R.id.homeStatus).apply {
             status?.let {
-                text = when(it){
+                when (it) {
                     "Connected" -> {
-                        "Connect Success"
+                        text = "Connect Success"
+                        imageResult.setBackgroundResource(R.drawable.sucess)
                     }
                     "Stopped" -> {
-                        "Stop Success"
+                        text = "Stop Success"
+                        imageResult.setBackgroundResource(R.drawable.fali)
                     }
                     else -> "Unknown Status"
                 }
-            }?: kotlin.run {
-                text= "Unknown Status"
+            } ?: kotlin.run {
+                text = "Unknown Status"
             }
         }
     }
